d3e899c7ec99fcbd417352bf545a93bb7d7e695a
Zach <26696119+bergeo-ogg@users.noreply.github.com>
2021-07-30 19:33:23 +0100

    Update timestamp_validity.py

--- bot/utils/timestamp_validity.py
+++ bot/utils/timestamp_validity.py
@@ -2,27 +2,27 @@ from bot.constants import TIMESTAMP_MAKE_BOUNDARIES, TIMESTAMP_RELATIVE_BOUNDARI
 from copy import deepcopy
 
 
-def make_data_validity(year: int, month: int, day: int, hour: int, minute: int, utc: int):
+def make_data_validity(input_data: dict):
     invalid_fields = []
     boundaries = deepcopy(TIMESTAMP_MAKE_BOUNDARIES)
     for item in boundaries:
-        input_value = locals()[item["name"]]
+        input_value = input_data[item["name"]]
         if item["min"] > input_value:
             item["input"] = input_value
             invalid_fields.append(item)
         if item["name"] == "day":
-            item["max"] = item["max"](year, month)
+            item["max"] = item["max"](input_data["year"], input_data["month"])
         if item["max"] < input_value:
             item["input"] = input_value
             invalid_fields.append(item)
     return invalid_fields
 
 
-def relative_data_validity(days: int, hours: int, minutes: int):
+def relative_data_validity(input_data: dict):
     invalid_fields = []
     boundaries = deepcopy(TIMESTAMP_RELATIVE_BOUNDARIES)
     for item in boundaries:
-        input_value = locals()[item["name"]]
+        input_value = input_data[item["name"]]
         if item["range"] < input_value:
             item["input"] = input_value
             invalid_fields.append(item)
