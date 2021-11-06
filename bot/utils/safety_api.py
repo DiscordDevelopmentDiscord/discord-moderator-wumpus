5112b2e31ea488902c4daca2d6fa544817b5714d
Zach <26696119+bergeo-ogg@users.noreply.github.com>
2021-11-06 20:26:59 +0000

    Create safety_api.py

---
+++ bot/utils/safety_api.py
@@ -0,0 +1,41 @@
+import aiohttp
+from urllib import parse
+from loguru import logger
+
+from bot.constants import SAFETY_API_ENDPOINT, SAFETY_IDS
+
+
+def key_sort(article_id: int):
+    try:
+        category_index = SAFETY_IDS.index(article_id)
+    except ValueError:
+        category_index = 2000  # Number which shall never be reached in articles
+    return category_index
+
+
+async def get_api(query_string: str = None, category: int = None):
+    url_params = {}
+    if query_string:
+        url_params["query"] = f"{{{query_string}}}"
+
+    if category:
+        url_params["section"] = category
+
+    request_url = f"{SAFETY_API_ENDPOINT}?{parse.urlencode(url_params)}"
+
+    results_parse = []
+    try:
+        async with aiohttp.ClientSession() as sess:
+            response = await sess.get(request_url)
+            response.raise_for_status()
+            data = await response.json()
+
+            results = data["results"]
+            for result in results:
+                result["url"] = f"https://discord.com/safety/{result['id']}"
+
+            results_parse = sorted(results, key=lambda x: key_sort(x["id"]))
+
+    except Exception:
+        logger.exception("Error fetching information from Safety")
+    return results_parse
