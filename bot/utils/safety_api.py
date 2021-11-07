7dc3d5e5f68d9b2ad7269f8ee76cbe1a0584e095
Zach <26696119+bergeo-ogg@users.noreply.github.com>
2021-11-07 16:49:09 +0000

    User feedback and slightly clearer logging message

--- bot/utils/safety_api.py
+++ bot/utils/safety_api.py
@@ -1,6 +1,7 @@
 import aiohttp
 from urllib import parse
 from loguru import logger
+from discord_slash.context import SlashContext
 
 from bot.constants import SAFETY_API_ENDPOINT, SAFETY_IDS
 
@@ -13,7 +14,7 @@ def key_sort(article_id: int):
     return category_index
 
 
-async def get_api(query_string: str = None, category: int = None):
+async def get_api(ctx: SlashContext, query_string: str = None, category: int = None):
     url_params = {}
     if query_string:
         url_params["query"] = f"{{{query_string}}}"
@@ -23,7 +24,6 @@ async def get_api(query_string: str = None, category: int = None):
 
     request_url = f"{SAFETY_API_ENDPOINT}?{parse.urlencode(url_params)}"
 
-    results_parse = []
     try:
         async with aiohttp.ClientSession() as sess:
             response = await sess.get(request_url)
@@ -37,5 +37,10 @@ async def get_api(query_string: str = None, category: int = None):
             results_parse = sorted(results, key=lambda x: key_sort(x["id"]))
 
     except Exception:
-        logger.exception("Error fetching information from Safety")
+        logger.exception("Error in Safety API Request")
+        await ctx.send(
+            "Couldn't load search results from the Safety Portal, please contact the devs"
+        )
+        return True
+
     return results_parse
